fefe0a8f528f12c7ddf1d2b50a95b9ccce2bbe2b
boreq <boreq@0x46.net>
2025-11-25 19:26:38 +0100

    Make an error message cover the stuff behind it

--- clacks-frontend/src/views/HomeView.vue
+++ clacks-frontend/src/views/HomeView.vue
@@ -247,13 +247,17 @@ h1 {
 .visualisation {
   position: relative;
 
-  .loading-indicator {
+  >.loading-indicator, >.error {
     background-color: $color-dark;
     position: absolute;
     left: 0;
     right: 0;
     top: 0;
     bottom: 0;
+  }
+
+  .error {
+    font-size: 3em;
   }
 }
 
