3673c3d83bca1fd8d2a8aaf2de6caaae5dc62f86
boreq <boreq@0x46.net>
2025-11-28 22:10:30 +0100

    Those comments are not useful

--- led-controller/led-controller.ino
+++ led-controller/led-controller.ino
@@ -1,10 +1,10 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 16      /* The amount of pixels/leds you have */
-#define DATA_PIN 9       /* The pin your data line is connected to */
-#define LED_TYPE WS2812B /* I assume you have WS2812B leds, if not just change it to whatever you have */
-#define BRIGHTNESS 255   /* Control the brightness of your leds */
-#define SATURATION 255   /* Control the saturation of your leds */
+#define NUM_LEDS 16
+#define DATA_PIN 9
+#define LED_TYPE WS2812B
+#define BRIGHTNESS 255
+#define SATURATION 255
 
 CRGB leds[NUM_LEDS];
 
@@ -18,6 +18,6 @@ void loop() {
       leds[i] = CHSV(i - (j * 2), SATURATION, BRIGHTNESS); /* The higher the value 4 the less fade there is and vice versa */ 
     }
     FastLED.show();
-    delay(25); /* Change this to your hearts desire, the lower the value the faster your colors move (and vice versa) */
+    delay(25);
   }
 }
