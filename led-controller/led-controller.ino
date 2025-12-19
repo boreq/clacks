8d09dcc95d72210028c5df9129b35fedcaa14ee1
boreq <boreq@0x46.net>
2025-12-19 17:10:31 +0100

    Animate all leds

--- led-controller/led-controller.ino
+++ led-controller/led-controller.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 16
-#define DATA_PIN 9
+#define NUM_LEDS 96
+#define DATA_PIN 8
 #define LED_TYPE WS2812B
 #define BRIGHTNESS 255
 #define SATURATION 255
@@ -15,7 +15,7 @@ void setup() {
 void loop() {
   for (int j = 0; j < 255; j++) {
     for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CHSV(i - (j * 2), SATURATION, BRIGHTNESS); /* The higher the value 4 the less fade there is and vice versa */ 
+      leds[i] = CHSV(i - (j * 2), SATURATION, BRIGHTNESS); /* The higher the mult value the less fade there is and vice versa */ 
     }
     FastLED.show();
     delay(25);
